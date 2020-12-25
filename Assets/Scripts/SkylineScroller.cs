25830e59bfc31a9401222d5bfb5fa0118c1a946b
Berke Aksoy <berkeaksoj@gmail.com>
2020-12-26 02:41:35 +0300

    Boss Scene Second Push

---
+++ Assets/Scripts/SkylineScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkylineScroller : MonoBehaviour
+{
+
+    [SerializeField] float skylineScrollSpeed = 0.2f;
+    Material myMaterial;
+    Vector2 offset;
+
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(skylineScrollSpeed, 0f);
+    }
+
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;
+    }
+}

cc8c9637f61eb8396d2d60969b24753480ab2e2f
KaanUstundag <37988497+Maestroymn@users.noreply.github.com>
2020-12-26 00:01:11 +0300

    UI manager added & main menu ui been created
    Leantween and TMPro packages added to the project.

---
+++ Assets/Scripts/Behaviours/MainMenuSoundBarBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Behaviours
+{
+    public class MainMenuSoundBarBehaviour : MonoBehaviour
+    {
+        [SerializeField] private float _maxXValue,_minXValue;
+        private LTDescr _descr;
+        private float _lerpedX,_currentX;
+        private bool _changing;
+        
+        public void ChangeBarScale()
+        {
+            if(!_changing)
+            {
+                _changing = true;
+                _currentX = Random.Range(_minXValue, _maxXValue);
+                float percentage = _currentX / _maxXValue;
+                _lerpedX = Mathf.Lerp(_minXValue, _maxXValue, percentage);
+                _descr = LeanTween.scaleX(gameObject, _lerpedX, 0.1f).setOnComplete(() => { _changing = false; });
+            }
+        }
+
+        public void SongPlay()
+        {
+            
+        }
+    }
+}

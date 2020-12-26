6a9173f6e15c116a1faeec790e658ebf3bc5ec6b
TOnurhan <onurhantopkara@gmail.com>
2020-12-27 01:37:35 +0300

    Healthbar prefab and script added.

---
+++ Assets/Scripts/BossHealthManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class BossHealthManager : MonoBehaviour
+{
+    [SerializeField] int maximum;
+    [SerializeField] int current;
+    [SerializeField] Image mask;
+    [SerializeField] List<float> percentageValues;
+    public event Action percentageReached;
+    
+    void Start()
+    {
+        for (int i = 0; i < percentageValues.Count; i++)
+        {
+            percentageValues[i] = percentageValues[i] / (float)maximum;
+        }
+    }
+
+    void GetCurrentFill()
+    {
+        float fillAmount = (float)current / (float)maximum;
+        mask.fillAmount = fillAmount;
+        for (int i = 0; i < percentageValues.Count; i++)
+        {
+            if(percentageValues[i] <= fillAmount && percentageValues[i - 1] > fillAmount && i >= 1)
+            {
+                percentageReached?.Invoke();
+            }
+        }
+    }
+}

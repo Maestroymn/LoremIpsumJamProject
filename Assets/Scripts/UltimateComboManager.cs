d4c8231b4018d2cdb41ddb520cf793a63e461009
TOnurhan <onurhantopkara@gmail.com>
2020-12-27 14:16:09 +0300

    Ulti Combo Manager added

---
+++ Assets/Scripts/UltimateComboManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UltimateComboManager : MonoBehaviour
+{
+    [SerializeField] int current;
+    [SerializeField] int maximum;
+    [SerializeField] Image mask;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ComboBarProgression();
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            ComboFailed();
+        }
+    }
+    public void ComboBarProgression()
+    {
+        current++;
+        float fillAmount = (float)current / (float)maximum;
+        mask.fillAmount = fillAmount;
+        if(mask.fillAmount == 1)
+        {
+            mask.fillAmount = 0;
+            current = 0;
+        }
+    }
+
+    public void ComboFailed()
+    {
+        mask.fillAmount = 0;
+        current = 0;
+    }
+}
